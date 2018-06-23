d2a5f37270ef9198658021722555135244ad1819
Shogun <shogun@cowtech.it>
2018-06-23 08:19:42 +0200

    fix: Export all files.

---
+++ dist/types/modules/types.d.ts
@@ -0,0 +1,89 @@
+/// <reference types="node" />
+import { Configuration, Entry, EntryFunc, ExternalsElement, Options as WebpackOptions, Plugin, RuleSetRule } from 'webpack';
+import { Babel, Externals, LibraryTarget, Plugins, Rules, Target } from './types';
+export declare type Entries = string | Array<string> | Entry | EntryFunc;
+export declare type Externals = ExternalsElement | Array<ExternalsElement>;
+export declare type Target = 'web' | 'webworker' | 'node' | 'async-node' | 'node-webkit' | 'atom' | 'electron' | 'electron-renderer' | 'electron-main' | ((compiler?: any) => void);
+export declare type LibraryTarget = 'var' | 'this' | 'commonjs' | 'commonjs2' | 'amd' | 'umd' | 'window' | 'assign' | 'jsonp';
+export interface Output {
+    filename?: string;
+    publicPath?: string;
+    target?: Target;
+    libraryTarget?: LibraryTarget;
+}
+export interface Environment {
+    environment: string;
+    version: string;
+    serviceWorkerEnabled: boolean;
+    [key: string]: any;
+}
+export interface Rules {
+    additional?: Array<RuleSetRule>;
+    babel?: boolean;
+    typescript?: boolean;
+    react?: boolean;
+    images?: boolean;
+    manifest?: boolean;
+    sitemap?: boolean;
+    robots?: boolean;
+    afterHook?(configuration: Array<RuleSetRule>): Promise<Array<RuleSetRule>>;
+}
+export interface Plugins {
+    additional?: Array<Plugin>;
+    concatenate?: boolean;
+    minify?: boolean;
+    hotModuleReload?: boolean;
+    commonChunks?: boolean;
+    analyze?: boolean | string;
+    afterHook?(plugins: Array<Plugin>): Promise<Array<Plugin>>;
+}
+export interface IconsToLoad {
+    [key: string]: Array<string>;
+}
+export interface Icons {
+    tags: {
+        [key: string]: string;
+    };
+    definitions: string;
+}
+export interface ServiceWorker {
+    enabled?: boolean;
+    src?: string;
+    dest?: string;
+    options?: object;
+}
+export interface Server {
+    host?: string;
+    port?: number;
+    https?: boolean | {
+        [key: string]: string | Buffer;
+    };
+    compress?: boolean;
+    hot?: boolean;
+    afterHook?(configuration: Server): Promise<Server>;
+}
+export interface Babel {
+    browsersWhiteList?: Array<string>;
+    exclude?: Array<string>;
+    modules?: boolean;
+    configuration?: any;
+}
+export interface Options extends Output {
+    environment?: string;
+    version?: string;
+    env?: Environment;
+    entries?: Entries;
+    index?: boolean | string;
+    rules?: Rules;
+    plugins?: Plugins;
+    icons?: IconsToLoad | Icons;
+    serviceWorker?: ServiceWorker;
+    srcFolder?: string;
+    destFolder?: string;
+    sourceMaps?: WebpackOptions.Devtool;
+    externals?: Externals;
+    server?: Server;
+    babel?: Babel;
+    uglify?: object;
+    afterHook?(configuration: Configuration): Promise<Configuration>;
+}
