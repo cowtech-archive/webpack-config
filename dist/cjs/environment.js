1232e23b00860eb6970800b9ce8ba59e0007bba1
Paolo Insogna <paolo@cowtech.it>
2021-01-10 07:33:12 +0100

    feat: Use YAML for configuration files.

--- dist/cjs/environment.js
+++ dist/cjs/environment.js
@@ -2,18 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.runHook = exports.setupEnvironment = void 0;
 const fs_1 = require("fs");
+const js_yaml_1 = require("js-yaml");
 const path_1 = require("path");
 function setupEnvironment(options) {
-    var _a, _b, _c, _d, _e, _f, _g;
-    const packageInfo = JSON.parse(fs_1.readFileSync(path_1.resolve(process.cwd(), './package.json'), 'utf-8'));
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j;
     const environment = options.environment;
+    let commonSettings = {};
+    let environmentSettings = {};
+    // Load settings from config/application.yml or from site key in package.json
+    const yamlPath = path_1.resolve(process.cwd(), './config/application.yml');
+    // Load from YAML file
+    if (fs_1.existsSync(yamlPath)) {
+        const configuration = js_yaml_1.load(fs_1.readFileSync(yamlPath, 'utf-8'));
+        commonSettings = (_a = configuration.common) !== null && _a !== void 0 ? _a : {};
+        environmentSettings = (_b = configuration[environment]) !== null && _b !== void 0 ? _b : {};
+        // Legacy package.json based configuration
+    }
+    else {
+        const packageInfo = JSON.parse(fs_1.readFileSync(path_1.resolve(process.cwd(), './package.json'), 'utf-8'));
+        commonSettings = (_d = (_c = packageInfo.site) === null || _c === void 0 ? void 0 : _c.common) !== null && _d !== void 0 ? _d : {};
+        environmentSettings = (_f = (_e = packageInfo.site) === null || _e === void 0 ? void 0 : _e[environment]) !== null && _f !== void 0 ? _f : {};
+    }
     return {
         environment,
         version: options.version,
-        serviceWorkerEnabled: (_b = (_a = options === null || options === void 0 ? void 0 : options.serviceWorker) === null || _a === void 0 ? void 0 : _a.enabled) !== null && _b !== void 0 ? _b : options.environment === 'production',
-        ...((_d = (_c = packageInfo.site) === null || _c === void 0 ? void 0 : _c.common) !== null && _d !== void 0 ? _d : {}),
-        ...((_f = (_e = packageInfo.site) === null || _e === void 0 ? void 0 : _e[environment]) !== null && _f !== void 0 ? _f : {}),
-        ...((_g = options.additionalEnvironment) !== null && _g !== void 0 ? _g : {})
+        serviceWorkerEnabled: (_h = (_g = options === null || options === void 0 ? void 0 : options.serviceWorker) === null || _g === void 0 ? void 0 : _g.enabled) !== null && _h !== void 0 ? _h : options.environment === 'production',
+        ...commonSettings,
+        ...environmentSettings,
+        ...((_j = options.additionalEnvironment) !== null && _j !== void 0 ? _j : {})
     };
 }
 exports.setupEnvironment = setupEnvironment;
--- src/environment.ts
+++ src/environment.ts
@@ -1,17 +1,35 @@
-import { readFileSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
+import { load } from 'js-yaml'
 import { resolve } from 'path'
 import { Environment, Hook, Options } from './types'
 
 export function setupEnvironment(options: Options): Environment {
-  const packageInfo = JSON.parse(readFileSync(resolve(process.cwd(), './package.json'), 'utf-8'))
   const environment = options.environment as string
+
+  let commonSettings: object = {}
+  let environmentSettings: object = {}
+
+  // Load settings from config/application.yml or from site key in package.json
+  const yamlPath = resolve(process.cwd(), './config/application.yml')
+
+  // Load from YAML file
+  if (existsSync(yamlPath)) {
+    const configuration = load(readFileSync(yamlPath, 'utf-8')) as { [key: string]: object | undefined }
+    commonSettings = configuration.common ?? {}
+    environmentSettings = configuration[environment] ?? {}
+    // Legacy package.json based configuration
+  } else {
+    const packageInfo = JSON.parse(readFileSync(resolve(process.cwd(), './package.json'), 'utf-8'))
+    commonSettings = packageInfo.site?.common ?? {}
+    environmentSettings = packageInfo.site?.[environment] ?? {}
+  }
 
   return {
     environment,
     version: options.version as string,
     serviceWorkerEnabled: options?.serviceWorker?.enabled ?? options.environment === 'production',
-    ...(packageInfo.site?.common ?? {}),
-    ...(packageInfo.site?.[environment] ?? {}),
+    ...commonSettings,
+    ...environmentSettings,
     ...(options.additionalEnvironment ?? {})
   }
 }
